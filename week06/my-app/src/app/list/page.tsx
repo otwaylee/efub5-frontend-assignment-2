4cb3bd61dd35fec78f3a4ed2f88777c36cd86921
otwaylee <ahyeong858@gmail.com>
2025-11-08 11:03:32 +0900

    feat: 게시글 리스트

--- week06/my-app/src/app/list/page.tsx
+++ week06/my-app/src/app/list/page.tsx
@@ -1,18 +1,22 @@
-import type Post from '@/models/post';
-import type { WithId } from 'mongodb';
-import axios from 'axios';
-import Link from 'next/link';
+export const dynamic = "force-dynamic";
+import axios from "axios";
+import type { WithId } from "mongodb";
+
+import type Post from "@/models/post";
+
+import ListItem from "./ListItem";
+import Link from "next/link";
 
 const List = async () => {
   const readPostList = async (): Promise<WithId<Post>[]> => {
     try {
       const response = await axios.get(
-        'http://localhost:3000/api/post/readList'
+        "http://localhost:3000/api/post/readList"
       );
       return response.data;
     } catch (error) {
       if (error instanceof Error) throw new Error(error.message);
-      else throw new Error('알 수 없는 오류가 발생했습니다.');
+      else throw new Error("알 수 없는 오류가 발생했습니다.");
     }
   };
 
@@ -20,15 +24,15 @@ const List = async () => {
   // posts를 활용해서 JSX 반환
 
   return (
-    <ul>
-      {posts.map((item, index) => (
-        <li key={index}>
-          <Link href={`/detail/${item._id}`}>
-            <h4>{item.title}</h4>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <div className="flex flex-col items-end gap-2">
+      <Link
+        href={"/write"}
+        className="w-fit rounded border bg-amber-200 px-2 py-1"
+      >
+        글쓰기
+      </Link>
+      <ListItem result={posts} />
+    </div>
   );
 };
 
